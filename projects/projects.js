cb01ec22d3a008462e7af7e7b52dfdbdbfa448b5
striaya <shenggaryoselino@gmail.com>
2026-01-27 18:21:35 +0700

    Portofolio

--- projects/projects.js
+++ projects/projects.js
@@ -7,3 +7,26 @@
       }
     });
   });
+
+   const hamburger = document.getElementById("hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const dropdowns = document.querySelectorAll(".dropdown > a");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+
+  dropdowns.forEach(drop => {
+    drop.addEventListener("click", (e) => {
+      if (window.innerWidth <= 768) {
+        e.preventDefault();
+        drop.parentElement.classList.toggle("active");
+      }
+    });
+  });
+
+    window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    navMenu.classList.remove("show");
+  }
+});
--- script.js
+++ script.js
@@ -97,3 +97,26 @@ function showSkill(skill) {
 function closeDetail() {
   document.getElementById("skillDetail").style.display = "none";
 }
+
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const dropdowns = document.querySelectorAll(".dropdown > a");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+
+  dropdowns.forEach(drop => {
+    drop.addEventListener("click", (e) => {
+      if (window.innerWidth <= 768) {
+        e.preventDefault();
+        drop.parentElement.classList.toggle("active");
+      }
+    });
+  });
+
+  window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    navMenu.classList.remove("show");
+  }
+});
